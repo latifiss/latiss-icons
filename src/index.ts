1fbdbcbe09844c0372306e95e6b5c98dded185bb
latifiss <latifissaka069@gmail.com>
2025-01-25 12:24:36 +0000

    feat: :construction: Added entry file

---
+++ src/index.ts
@@ -0,0 +1,76 @@
+export { default as AlertAdd } from "../src/components/icons/alertAdd";
+export { default as Alien } from "../src/components/icons/alien";
+export { default as Battery } from "../src/components/icons/battery";
+export { default as Bluetooth } from "../src/components/icons/bluetooth";
+export { default as Brush } from "../src/components/icons/brush";
+export { default as Calendar } from "../src/components/icons/calendar";
+export { default as Camera } from "../src/components/icons/camera";
+export { default as ChargeBattery } from "../src/components/icons/chargeBattery";
+export { default as Chef } from "../src/components/icons/chef";
+export { default as ClosedCaption } from "../src/components/icons/closedCaption";
+export { default as Cupboard } from "../src/components/icons/cupboard";
+export { default as Cursor } from "../src/components/icons/cursor";
+export { default as DisplayMode } from "../src/components/icons/displayMode";
+export { default as Download } from "../src/components/icons/download";
+export { default as Expand } from "../src/components/icons/expand";
+export { default as FaceScan } from "../src/components/icons/faceScan";
+export { default as Filter } from "../src/components/icons/filter";
+export { default as FolderMultiple } from "../src/components/icons/folderMultiple";
+export { default as Format } from "../src/components/icons/format";
+export { default as Hand } from "../src/components/icons/hand";
+export { default as HelpMessage } from "../src/components/icons/helpMessage";
+export { default as IceCream } from "../src/components/icons/icecream";
+export { default as Laptop } from "../src/components/icons/laptop";
+export { default as LightBulb } from "../src/components/icons/lightBulb";
+export { default as Print } from "../src/components/icons/print";
+export { default as Send } from "../src/components/icons/send";
+export { default as Setting } from "../src/components/icons/setting";
+export { default as Shield } from "../src/components/icons/shield";
+export { default as Signal } from "../src/components/icons/signal";
+export { default as Skull } from "../src/components/icons/skull";
+export { default as Sparkle } from "../src/components/icons/sparkle";
+export { default as Synchronize } from "../src/components/icons/synchronize";
+export { default as Telescope } from "../src/components/icons/telescope";
+export { default as Thumbprint } from "../src/components/icons/thumbprint";
+export { default as Transfer } from "../src/components/icons/transfer";
+export { default as User } from "../src/components/icons/user";
+export { default as Watermelon } from "../src/components/icons/watermelon";
+export { default as Wifi } from "../src/components/icons/wifi";
+export { default as Wine } from "../src/components/icons/wine";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
